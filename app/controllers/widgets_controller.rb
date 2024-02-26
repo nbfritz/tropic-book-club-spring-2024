61ade407bc94ed4d1cf136f5d1c81802675d79f3
Nathan Fritz <nbfritz@gmail.com>
2024-02-26 11:20:30 -0600

    Section 17-05 edits.

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,43 @@
+class WidgetsController < ApplicationController
+  def new
+    @widget = Widget.new
+
+    @manufacturers = Manufacturer.all
+  end
+
+  def create
+    widget_params = params.require(:widget).permit(
+      :name, :price_cents, :manufacturer_id
+    )
+
+# START:edit:3
+    if widget_params[:price_cents].present?
+      widget_params[:price_cents] = (
+        BigDecimal(widget_params[:price_cents]) * 100
+      ).to_i
+    end
+
+# END:edit:3
+    result = WidgetCreator.new.create_widget(
+               Widget.new(widget_params)
+    )
+
+    if result.created?
+      redirect_to widget_path(result.widget)
+    else
+      @widget = result.widget
+      @manufacturers = Manufacturer.all
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @widget = Widget.find(params[:id])
+  end
+  def index
+    @widgets = [
+      OpenStruct.new(id: 1234, name: "Stembolt"),
+      OpenStruct.new(id: 2, name: "Flux Capacitor"),
+    ]
+  end
+end
---
+++ app/models/widget.rb
@@ -0,0 +1,26 @@
+class Widget < ApplicationRecord
+  belongs_to :manufacturer
+
+  def user_facing_identifier
+    id_as_string = self.id.to_s
+    if id_as_string.length < 3
+      return id_as_string
+    end
+
+    "%{first}.%{last_two}" % {
+      first: id_as_string[0..-3],
+      last_two: id_as_string[-2..-1]
+    }
+  end
+  belongs_to :widget_status
+# START:edit:3
+  validates :name, {
+    presence: true,
+    length: { minimum: 5 }
+  }
+# END:edit:3
+  validates :manufacturer_id, { presence: true }
+  validates :price_cents,
+    numericality: { less_than_or_equal_to: 10_000_00 }
+  normalizes :name, with: ->(name) { name.blank? ? nil : name }
+end
