d61d399210263dd43059aadcb406b82f9d64fef2
Nathan Fritz <nbfritz@gmail.com>
2024-02-26 11:20:31 -0600

    Section 18-04 edits.

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+if !Rails.env.development?
+  puts "[ db/seeds.rb ] Seed data is for development only, " + 
+       "not #{Rails.env}"
+  exit 0
+end
+
+require "factory_bot"
+
+Widget.destroy_all
+Manufacturer.destroy_all
+Address.destroy_all
+WidgetStatus.destroy_all
+
+puts "[ db/seeds.rb ] Creating development data..."
+FactoryBot.create(:widget_status, name: "Fresh")
+10.times do
+  FactoryBot.create(:manufacturer)
+end
+cyberdyne = FactoryBot.create(:manufacturer,
+                              name: "Cyberdyne Systems")
+FactoryBot.create(:widget, name: "Stembolt",
+                           manufacturer: cyberdyne)
+puts "[ db/seeds.rb ] Done"
---
+++ test/test_helper.rb
@@ -0,0 +1,27 @@
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+Capybara.configure do |config|
+  # This allows helpers like click_on to locate
+  # any object by data-testid in addition to
+  # built-in selector-like values
+  config.test_id = "data-testid"
+end
+
+require "support/confidence_check"
+
+module ActiveSupport
+  class TestCase
+# START:edit:3
+    include TestSupport::ConfidenceCheck
+
+# END:edit:3
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
+
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+  end
+end
