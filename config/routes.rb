7f37829c3a9de36c55b4d73672ac6878bc0569ae
Nathan Fritz <nbfritz@gmail.com>
2024-02-26 11:20:20 -0600

    Section 06-01 edits.

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :widgets
+
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+# START:edit:3
+  get "manufacturer/:id", to: "manufacturers#show"
+# END:edit:3
+end
